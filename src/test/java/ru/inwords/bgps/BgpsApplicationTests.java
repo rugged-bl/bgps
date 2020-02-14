08c281f58955b500f9141ddaa228dc36e5c05d6b
rugged-bl <vasia.shumilov@gmail.com>
2020-02-14 12:57:01 +0300

    Initial commit

---
+++ src/test/java/ru/inwords/bgps/BgpsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.inwords.bgps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BgpsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
